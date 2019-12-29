7993375d53326207da5ee10771cd73fdd951d121
MinaFes <ribeirocatarina72@gmail.com>
2019-12-29 13:11:21 +0100

    amélioration 2 : séparation de PaintingAnts en 2 classes

--- src/main/java/org/polytechtours/javaperformance/tp/paintingants/CPainting.java
+++ src/main/java/org/polytechtours/javaperformance/tp/paintingants/CPainting.java
@@ -12,8 +12,6 @@ import java.awt.Rectangle;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-import org.openjdk.jmh.annotations.Benchmark;
-
 /**
  * <p>
  * Titre : Painting Ants
